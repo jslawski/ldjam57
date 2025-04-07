80e5a61a12eedbc8e66e1762fc7beff41da926ee
Jared Slawski <jaredslawski@gmail.com>
2025-04-07 01:14:44 -0600

    Collectibles implemented

---
+++ Assets/_Scripts/ScoreManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public static class ScoreManager
+{
+
+    public static float timeElapsed = 0.0f;
+    public static int collectiblesGrabbed = 0;
+    public static int totalTimesDucked = 0;
+
+
+    public static void ResetScores()
+    {
+        ScoreManager.timeElapsed = 0.0f;
+        ScoreManager.collectiblesGrabbed = 0;
+        ScoreManager.totalTimesDucked = 0;
+    }
+
+    public static void IncrementTimesDucked()
+    {
+        ScoreManager.totalTimesDucked++;
+    }
+
+    public static void IncrementTime()
+    {
+        ScoreManager.timeElapsed += Time.fixedDeltaTime;
+    }
+
+    public static void IncrementCollectiblesGrabbed()
+    {
+        ScoreManager.collectiblesGrabbed++;
+    }
+}
